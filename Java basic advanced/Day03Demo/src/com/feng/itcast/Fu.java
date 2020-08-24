9d2acfa3a64d54d1ac8c3a5ddaab6a07ca8ae468
Anthony-bird <1765632807@qq.com>
2020-08-25 00:00:23 +0800

    java05

---
+++ Java basic advanced/Day03Demo/src/com/feng/itcast/Fu.java
@@ -0,0 +1,29 @@
+package com.feng.itcast;
+
+public class Fu {
+    // 1.private 只能本类中访问
+    private void show1(){
+        System.out.println("private");
+    }
+    // 2.缺省：本类，同一个包下的类中。
+    void show2(){
+        System.out.println("缺省");
+    }
+
+    // 3.protected：本类，同一个包下的类中，其他包下的子类
+    protected void show3(){
+        System.out.println("protected");
+    }
+    // 4.任何地方都可以
+    public void show4(){
+        System.out.println("public");
+    }
+
+    public static void main(String[] args) {
+        Fu fu = new Fu();
+        fu.show1();
+        fu.show2();
+        fu.show3();
+        fu.show4();
+    }
+}
