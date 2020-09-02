a2da7c0020a483d39ca2719845af3cea20a95e38
Anthony-bird <1765632807@qq.com>
2020-09-03 00:22:18 +0800

    java15

---
+++ Java basic advanced/Day09Demo/src/com/feng/_13Stream流的综合应用/Student.java
@@ -0,0 +1,25 @@
+package com.feng._13Stream流的综合应用;
+
+public class Student {
+    private String name ;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
+
